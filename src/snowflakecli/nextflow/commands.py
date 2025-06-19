d54fc62dacdef4e10c43ab5722192e1c05d07a99
Haowei Yu <haowei.yu@snowflake.com>
2025-06-19 05:52:08 +0000

    More changes

--- src/snowflakecli/nextflow/commands.py
+++ src/snowflakecli/nextflow/commands.py
@@ -1,10 +1,30 @@
 import typer
 from snowflake.cli.api.commands.snow_typer import SnowTyperFactory
 from snowflake.cli.api.output.types import CommandResult, MessageResult
-from snowflakecli.nextflow.workflow.commands import app as workflow_app
+from snowflakecli.nextflow.manager import NextflowManager
 
 app = SnowTyperFactory(
     name="nextflow",
     help="Run Nextflow workflows in Snowpark Container Service",
 )
-app.add_typer(workflow_app)
+
+@app.command("run", requires_connection=True)
+def run_workflow(
+    project_dir: str = typer.Argument(
+        help="Name of the workflow to run"
+    ),
+    profile: str = typer.Option(
+        None,
+        "-profile",
+        help="Nextflow profile to use for the workflow execution",
+    ),
+    **options,
+) -> CommandResult:
+    """
+    Run a Nextflow workflow in Snowpark Container Service.
+    """
+
+    manager = NextflowManager(project_dir, profile)
+    manager.run()
+    # TODO: Implement the actual workflow execution logic
+    return MessageResult(f"Executed successfully!")
